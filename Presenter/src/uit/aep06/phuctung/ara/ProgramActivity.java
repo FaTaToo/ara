73aacbb76ca2e8766d732bb53a3347ca34242132
Tung Pham Tang <phamtangtung@gmail.com>
2015-11-25 01:38:59 +0700

    [Module Presetner] Update Program Activity

--- Presenter/src/uit/aep06/phuctung/ara/ProgramActivity.java
+++ Presenter/src/uit/aep06/phuctung/ara/ProgramActivity.java
@@ -1,34 +1,85 @@
 package uit.aep06.phuctung.ara;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import uit.aep06.phuctung.ara.Library.SlidingTabLayout;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
-public class ProgramActivity extends Activity {
-
+public class ProgramActivity extends FragmentActivity {
+	String CustomerID;
+	
+	ViewPager pager;
+    ViewPagerAdapter adapter;
+    SlidingTabLayout tabs;
+    CharSequence Titles[]={"All","Checked"};
+    int Numboftabs = 2;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_program);
+		
+//		// Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
+//        adapter =  new ViewPagerAdapter(getSupportFragmentManager(),Titles,Numboftabs);
+//		
+//        // Assigning ViewPager View and setting the adapter
+//        pager = (ViewPager) findViewById(R.id.pager);
+//        pager.setAdapter(adapter);
+// 
+//        // Assiging the Sliding Tab Layout View
+//        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
+//        tabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
+// 
+//        // Setting Custom Color for the Scroll bar indicator of the Tab View
+//        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+//            @Override
+//            public int getIndicatorColor(int position) {
+//                return getResources().getColor(R.color.tabsScrollColor);
+//            }
+//        });
+// 
+//        // Setting the ViewPager For the SlidingTabsLayout
+//        tabs.setViewPager(pager);
+		
+		
+//		Intent intent = getIntent();
+//		CustomerID = intent.getStringExtra("customerID");
+//		BackgroundTask task = new BackgroundTask();
+//		task.execute();
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.program, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+	private class BackgroundTask extends AsyncTask<Void, Void, Void> {
+		private ProgressDialog dialog;
+		
+		@Override
+	    protected void onPreExecute() {
+	    	dialog = new ProgressDialog(ProgramActivity.this);
+	    	dialog.setCancelable(false);
+	        dialog.setMessage("Waiting for loading....");
+	        dialog.show();
+	    }
+		
+		@Override
+	    protected void onPostExecute(Void result) {
+	        if (dialog.isShowing()) {
+	            dialog.dismiss();
+	        }
+	    }
+		
+		@Override
+		protected Void doInBackground(Void... params) {
+			// TODO Auto-generated method stub
+			return null;
 		}
-		return super.onOptionsItemSelected(item);
+		
 	}
 }
