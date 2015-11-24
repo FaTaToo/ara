9472da9e8a8feb0faa777a2e771fc0adc5c6d0d7
Tung Pham Tang <phamtangtung@gmail.com>
2015-11-25 01:37:09 +0700

    [Module Presenter] Add CustomerAccount class

---
+++ Presenter/src/uit/aep06/phuctung/ara/CommonClass/CustomerAccount.java
@@ -0,0 +1,40 @@
+package uit.aep06.phuctung.ara.CommonClass;
+
+public class CustomerAccount {
+    public int ID;
+	public String Fullname;
+	public String Email;
+	public String getFullname() {
+		return Fullname;
+	}
+	public void setFullname(String fullname) {
+		Fullname = fullname;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String Username;
+	public String Pass;
+	public String getUsername() {
+		return Username;
+	}
+	public void setUsername(String username) {
+		Username = username;
+	}
+	public String getPass() {
+		return Pass;
+	}
+	public void setPass(String pass) {
+		Pass = pass;
+	}
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}	
+}
+
