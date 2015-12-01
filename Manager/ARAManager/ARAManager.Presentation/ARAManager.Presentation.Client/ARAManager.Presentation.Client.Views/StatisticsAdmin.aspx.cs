e0b300ed1122413822b74d561f7d6eb3776f129f
phucls <11520288@gm.uit.edu.vn>
2015-12-02 05:53:52 +0700

    [Manager Module]-[HotFix] Implement Statistics page.

--- Manager/ARAManager/ARAManager.Presentation/ARAManager.Presentation.Client/ARAManager.Presentation.Client.Views/StatisticsAdmin.aspx.cs
+++ Manager/ARAManager/ARAManager.Presentation/ARAManager.Presentation.Client/ARAManager.Presentation.Client.Views/StatisticsAdmin.aspx.cs
@@ -11,7 +11,12 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.UI;
+using AjaxControlToolkit;
+using ARAManager.Common.Dto;
+using ARAManager.Presentation.Connectivity;
 
 namespace ARAManager.Presentation.Client.ARAManager.Presentation.Client.Views
 {
@@ -19,6 +24,71 @@ namespace ARAManager.Presentation.Client.ARAManager.Presentation.Client.Views
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            var customersList = ClientServiceFactory.CustomerService.GetAllCustomers();
+            LoadSexPieChart(customersList);
+            LoadAgeChart(GetAgeRanges(customersList));
+        }
+        /// <summary>
+        /// Load the values for customer sex pie chart
+        /// </summary>
+        private void LoadSexPieChart(IList<Customer> customers)
+        {
+            SexPieChart.PieChartClientValues.Add(new PieChartValue()
+            {
+                Category = "Male",
+                Data = customers.Count(c => c.Sex == "Male"),
+                PieChartValueColor = "green",
+                PieChartValueStrokeColor = "white"
+            });
+
+            SexPieChart.PieChartClientValues.Add(new PieChartValue()
+            {
+                Category = "Female",
+                Data = customers.Count(c => c.Sex == "Female"),
+                PieChartValueColor = "pink",
+                PieChartValueStrokeColor = "red"
+            });
+        }
+
+        private void LoadAgeChart(Dictionary<string, int> ageRanges)
+        {
+            foreach (var ageRange in ageRanges)
+            {
+                if (ageRange.Value > 0)
+                {
+                    AgePieChart.PieChartClientValues.Add(new PieChartValue()
+                    {
+                        Category = ageRange.Key,
+                        Data = ageRange.Value
+                    });
+                }
+            }
+        }
+
+        private Dictionary<string, int> GetAgeRanges(IList<Customer> customers)
+        {
+            var ageDic = new Dictionary<string, int>();
+            
+            ageDic.Add("1-20", customers.Count(c =>
+                GetAge(c.BirthDay) < 21
+            ));
+            ageDic.Add("21-30", customers.Count(c=>GetAge(c.BirthDay) > 20 && GetAge(c.BirthDay) < 31));
+            ageDic.Add("31-40", customers.Count(c => GetAge(c.BirthDay) > 30 && GetAge(c.BirthDay) < 41));
+            ageDic.Add("41-50", customers.Count(c => GetAge(c.BirthDay) > 40 && GetAge(c.BirthDay) < 51));
+            ageDic.Add("51-60", customers.Count(c => GetAge(c.BirthDay) > 50 && GetAge(c.BirthDay) < 61));
+            ageDic.Add(">60", customers.Count(c => GetAge(c.BirthDay) > 60));
+
+            return ageDic;
+        }
+
+        public Int32 GetAge(DateTime dateOfBirth)
+        {
+            var today = DateTime.Today;
+
+            var a = (today.Year * 100 + today.Month) * 100 + today.Day;
+            var b = (dateOfBirth.Year * 100 + dateOfBirth.Month) * 100 + dateOfBirth.Day;
+
+            return (a - b) / 10000;
         }
     }
 }
--- Manager/ARAManager/ARAManager.Presentation/ARAManager.Presentation.Client/ARAManager.Presentation.Client.Views/StatisticsCompany.aspx.cs
+++ Manager/ARAManager/ARAManager.Presentation/ARAManager.Presentation.Client/ARAManager.Presentation.Client.Views/StatisticsCompany.aspx.cs
@@ -11,14 +11,86 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 using System;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
 using System.Web.UI;
+using System.Web.UI.WebControls;
+using ARAManager.Presentation.Client.ARAManager.Presentation.Client.Common;
+using ARAManager.Presentation.Connectivity;
 
 namespace ARAManager.Presentation.Client.ARAManager.Presentation.Client.Views
 {
     public partial class StatisticsCompany : Page
     {
+
+        #region IFields
+
+        private readonly Validation m_validator = new Validation();
+
+        #endregion IFields
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            LoadGeneralStatistic();
+        }
+
+        private void LoadGeneralStatistic()
+        {
+            var campaignList =
+                ClientServiceFactory.CampaignService.GetAllCampaigns()
+                    .Where(c => c.Company.UserName == Page.User.Identity.Name).ToList();
+            var campaignNumbers = campaignList.Count();
+
+            string[] x = new string[campaignNumbers];
+            decimal[] y = new decimal[campaignNumbers];
+            int totalSubcription = 0;
+            for (int i = 0; i < campaignNumbers; i++)
+            {
+                x[i] = campaignList[i].CampaignName;
+                y[i] = campaignList[i].Subscriptions.Count;
+                totalSubcription += campaignList[i].Subscriptions.Count;
+            }
+
+            campaignChart.Series.Add(new AjaxControlToolkit.BarChartSeries { Data = y });
+            campaignChart.CategoriesAxis = string.Join(",", x);
+
+            lblSubcriptionNum.Text = string.Format("Total Subcriptions: {0}", totalSubcription);
+            lblCampaignNum.Text = string.Format("Total campaigns: {0}", campaignNumbers);
+        }
+
+        protected void CustomValidator_CampaignName_OnServerValidate(object source, ServerValidateEventArgs args)
+        {
+            CustomValidator_CampaignName.ErrorMessage = Validation.VALIDATOR_CAMPAIGN_NAME;
+            args.IsValid = m_validator.ValidateChar100(txtCampaignName.Text);
+        }
+
+        protected void CustomValidator_RequireFileds_OnServerValidate(object source, ServerValidateEventArgs args)
+        {
+            CustomValidator_RequireFileds.ErrorMessage = Validation.VALIDATOR_REQUIRED_CRITERION_SEARCH;
+            args.IsValid = !string.IsNullOrEmpty(txtCampaignName.Text);
+        }
+
+        protected void btnSearch_OnClick(object sender, EventArgs e)
+        {
+            LoadCampaignDetails();
+        }
+
+        private void LoadCampaignDetails()
+        {
+            var campaign = ClientServiceFactory.CampaignService
+                    .GetAllCampaigns().FirstOrDefault(c => c.Company.UserName == Page.User.Identity.Name && c.CampaignName == txtCampaignName.Text);
+            if (campaign != null)
+            {
+                lblCampaignName.Text = campaign.CampaignName;
+                lblTotalSub.Text = string.Format("{0} subcriptions", campaign.Subscriptions.Count());
+                lblCompleteSub.Text = campaign.Subscriptions.Count(c => c.IsComplete == true).ToString();
+                if (campaign.Subscriptions.Count > 0)
+                {
+                    lblAveRating.Text =
+                        campaign.Subscriptions.Average(c => c.Rating).ToString(CultureInfo.InvariantCulture);
+                }
+            }
         }
     }
 }
