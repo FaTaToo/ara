95c39cf463c70a37a7c27add3aeb7704a5992f17
phucls <11520288@gm.uit.edu.vn>
2015-12-15 19:41:13 +0700

    [Presenter Module] Test services.

---
+++ Manager/ARAManager/ARAManager.Test/ARAManager.Test.UnitTest/CustomerAccountServicesTest.cs
@@ -0,0 +1,22 @@
+﻿using ARAManager.Business.Service.Services.Presenter;
+using ARAManager.Common.PresenterJson;
+using NUnit.Framework;
+
+namespace ARAManager.Test.UnitTest
+{
+    [TestFixture]
+    public class CustomerAccountServicesTest
+    {
+        [Test]
+        public void Authenticate()
+        {
+            var customerAccount =new CustomerAccount();
+            Assert.AreEqual("Succes",customerAccount.Authenticate(
+                                new AuthenticationJsonRequest()
+                                {
+                                    UserName = "tma",
+                                    Password = "tma"
+                                }).Message);
+        }
+    }
+}
