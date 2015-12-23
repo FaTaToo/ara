99a8120eef32fe09a874a680568b28d80ebdb8d4
Tung Pham Tang <phamtangtung@gmail.com>
2015-12-24 00:15:26 +0700

    [Presenter module] Implement service for Sign up function

--- Presenter/App/src/uit/aep06/phuctung/ara/RegisterActivity.java
+++ Presenter/App/src/uit/aep06/phuctung/ara/RegisterActivity.java
@@ -1,8 +1,11 @@
 package uit.aep06.phuctung.ara;
 
+import org.json.JSONException;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -11,86 +14,123 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 import uit.aep06.phuctung.ara.CommonClass.CustomerAccount;
 import uit.aep06.phuctung.ara.Service.CustomerAccountService;
 
 public class RegisterActivity extends Activity implements OnClickListener {
 	Button btnRegister;
-	EditText etUser, etFullname, etEmail, etPass, etRetype;
+	EditText etUser, etFirstName, etLastName, etEmail, etPass, etRetype, etAddress, etBirthday, etPhone;
+	RadioButton radioMale;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_register);
-		
-		btnRegister = (Button)findViewById(R.id.btnRegister);
+
+		btnRegister = (Button) findViewById(R.id.btnRegister);
 		btnRegister.setOnClickListener(this);
 	}
+
 	@Override
 	public void onClick(View v) {
-		if(v.getId() == R.id.btnRegister) {
-			etUser = (EditText)findViewById(R.id.etUsername);
-			etFullname = (EditText)findViewById(R.id.etFullname);
-			etEmail = (EditText)findViewById(R.id.etEmail);
-			etPass = (EditText)findViewById(R.id.etPassword);
-			etRetype = (EditText)findViewById(R.id.etRetypePassword);
-			
-			if (etUser.getText().toString().isEmpty() || etFullname.getText().toString().isEmpty() ||
-					etEmail.getText().toString().isEmpty() || etPass.getText().toString().isEmpty()){				
-				Toast.makeText(getApplicationContext(), "Alert ! Can not be empty.", Toast.LENGTH_LONG).show();
-				return;
+		if (v.getId() == R.id.btnRegister) {
+			etFirstName = (EditText) findViewById(R.id.etFirstName);
+			etLastName = (EditText) findViewById(R.id.etLastName);
+			radioMale = (RadioButton) findViewById(R.id.radioMale);
+			etBirthday = (EditText) findViewById(R.id.etBirthday);
+			etAddress = (EditText) findViewById(R.id.etAddress);
+			etEmail = (EditText) findViewById(R.id.etEmail);
+			etPhone = (EditText) findViewById(R.id.etPhone);
+			etUser = (EditText) findViewById(R.id.etUsername);
+			etPass = (EditText) findViewById(R.id.etPassword);
+			etRetype = (EditText) findViewById(R.id.etRetypePassword);
+			if (etFirstName.getText().toString().isEmpty() || etLastName.getText().toString().isEmpty()
+					|| etBirthday.getText().toString().isEmpty() || etAddress.getText().toString().isEmpty()
+					|| etEmail.getText().toString().isEmpty() || etPhone.getText().toString().isEmpty()
+					|| etUser.getText().toString().isEmpty() || etPass.getText().toString().isEmpty()
+					|| etRetype.getText().toString().isEmpty())
+				Toast.makeText(getApplicationContext(), "Alert ! Can not be" + " empty.", Toast.LENGTH_LONG).show();
+			return;
+		}
+		if (!etPass.getText().toString().equals(etRetype.getText().toString())) {
+			etPass.setText("");
+			etRetype.setText("");
+			etPass.setFocusable(true);
+			etPass.setHighlightColor(new Color().RED);
+			etPass.setHint("Passwords do not match");
+		} else {
+			CustomerAccount customerAccount = new CustomerAccount();
+			customerAccount.setFirstName(etFirstName.getText().toString());
+			customerAccount.setLastName(etLastName.getText().toString());
+			customerAccount.setBirthDay(etBirthday.getText().toString());
+			customerAccount.setAddress(etAddress.getText().toString());
+			customerAccount.setEmail(etEmail.getText().toString());
+			customerAccount.setPhone(etPhone.getText().toString());
+			customerAccount.setUsername(etUser.getText().toString());
+			customerAccount.setPass(etPass.getText().toString());
+			if (radioMale.isChecked()) {
+				customerAccount.setSex("Male");
+			} else {
+				customerAccount.setSex("Female");
 			}
-			if (!etPass.getText().toString().equals(etRetype.getText().toString()))
-			{
-				etPass.setText("");
-				etRetype.setText("");
-				etPass.setFocusable(true);
-				etPass.setHighlightColor(new Color().RED);
-				etPass.setHint("Passwords do not match");
-			}else{
-				CustomerAccount customerAccount = new CustomerAccount();
-				customerAccount.setUsername(etUser.getText().toString());
-				customerAccount.setPass(etPass.getText().toString());
-				customerAccount.setEmail(etEmail.getText().toString());
-				customerAccount.setFullname(etFullname.getText().toString());
-				CustomerAccountService service = new CustomerAccountService();
-				try {
-					int result = 0;//service.AddNewCustomerAccount(customerAccount);
-					if (result == 0){
-						etUser.setText("");
-						etUser.setFocusable(true);
-						etUser.setHighlightColor(new Color().RED);
-						etUser.setHint("This username is exists.");
-						return;
-					}else{
-						Intent intent = new Intent (getApplicationContext(),LoginActivity.class);
-						startActivity(intent);
-					}
-				} catch (Exception e) {
-					Log.e("Register", "fail...");
+			
+			try {
+				SignUp task = new SignUp();
+				int result = task.execute(customerAccount).get();
+
+				if (result == 0) {
+					etUser.setText("fatatoo");
+					etUser.setFocusable(true);
+					etUser.setHighlightColor(new Color().RED);
+					etUser.setHint("This username or email or phone is exists.");
+					return;
+				} else {
+					Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+					startActivity(intent);
 				}
-			}			
+			} catch (Exception e) {
+				Log.e("Register", "fail...");
+			}
 		}
 	}
-	
+
+	private class SignUp extends AsyncTask<CustomerAccount, Void, Integer> {
+
+		@Override
+		protected Integer doInBackground(CustomerAccount... params) {
+			CustomerAccountService service = new CustomerAccountService();
+			int result = 0;
+			try {
+				result = service.addNewCustomerAccount(params[0]);
+				return result;
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				Log.e("Sign Up", e.toString());
+			}
+			return result;
+		}
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.menu, menu);
 		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		Intent intent;
-	    // Handle item selection
-	    switch (item.getItemId()) {	    	
-	        case R.id.about:
-	        	intent = new Intent(getApplicationContext(),AboutActivity.class);
-	    		startActivity(intent);
-	            return true;
-	        default:
-	            return super.onOptionsItemSelected(item);
-	    }
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.about:
+			intent = new Intent(getApplicationContext(), AboutActivity.class);
+			startActivity(intent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 }
--- Presenter/App/src/uit/aep06/phuctung/ara/Service/CustomerAccountService.java
+++ Presenter/App/src/uit/aep06/phuctung/ara/Service/CustomerAccountService.java
@@ -17,11 +17,71 @@ import uit.aep06.phuctung.ara.CommonClass.CustomerAccount;
 
 public class CustomerAccountService {
 
-	public int addNewCustomerAccount(CustomerAccount acc) {
-		return 0;
+	public int addNewCustomerAccount(CustomerAccount account) throws JSONException {
+		String targetURL = "http://ara288.somee.com/Services/Presenter/CustomerAccount.svc/SignUp";
+
+		URL url;
+		HttpURLConnection connection = null;
+
+		// Create JSONObject here
+		JSONObject jsonParam = new JSONObject();
+		jsonParam.put("FirstName", account.getFirstName());
+		jsonParam.put("LastName", account.getLastName());
+		jsonParam.put("Sex", account.getSex());
+		jsonParam.put("BirthDay", account.getBirthDay());
+		jsonParam.put("Address", account.getAddress());
+		jsonParam.put("Email", account.getEmail());
+		jsonParam.put("Phone", account.getPhone());
+		jsonParam.put("UserName", account.getUsername());
+		jsonParam.put("Password", account.getPass());
+
+		try {
+			// Create connection
+			url = new URL(targetURL);
+			connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("POST");
+			connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+			connection.setRequestProperty("Accept", "application/json");
+
+			connection.setUseCaches(false);
+			connection.setDoInput(true);
+			connection.setDoOutput(true);
+
+			// Send request
+			OutputStreamWriter wr = new OutputStreamWriter(connection.getOutputStream());
+			wr.write(jsonParam.toString());
+			wr.flush();
+
+			// Get Response
+			InputStream is = connection.getInputStream();
+			BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+			String line;
+
+			StringBuilder builder = new StringBuilder();
+			while ((line = rd.readLine()) != null) {
+				builder.append(line).append("\n");
+			}
+			rd.close();
+
+			JSONTokener tokener = new JSONTokener(builder.toString());
+			JSONObject resultObject = new JSONObject(tokener);
+			String result = resultObject.getString("Message");
+			if (result.equals("Success")) {
+				return 1;
+			} else {
+				return 0;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		} finally {
+			if (connection != null) {
+				connection.disconnect();
+			}
+		}
+
 	}
 
-	@SuppressWarnings("deprecation")
 	public int authenticate(CustomerAccount account) throws IOException, JSONException {
 		String targetURL = "http://ara288.somee.com/Services/Presenter/CustomerAccount.svc/Authenticate";
 
