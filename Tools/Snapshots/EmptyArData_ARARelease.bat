4f95626c1cf683a5820c1dc2f196f0c4d2d0d2ab
phucls <11520288@gm.uit.edu.vn>
2015-12-27 18:07:14 +0700

    [Debug tool]-[Reset data] Add local debug path of target Json.

--- Tools/Snapshots/EmptyArData_ARARelease.bat
+++ Tools/Snapshots/EmptyArData_ARARelease.bat
@@ -2,7 +2,7 @@ del /Q "D:\Projects\ARA\1.0\src\Manager\ARAManager\ARAManager.Presentation\ARAMa
 del /Q "D:\Projects\ARA\1.0\src\Manager\ARAManager\ARAManager.Presentation\ARAManager.Presentation.Client\Ara_Data\Campaigns\Banner\*"
 del /Q "D:\Projects\ARA\1.0\src\Manager\ARAManager\ARAManager.Presentation\ARAManager.Presentation.Client\Ara_Data\Missions\Avatar\*"
 del /Q "D:\Projects\ARA\1.0\src\Manager\ARAManager\ARAManager.Presentation\ARAManager.Presentation.Client\Ara_Data\Targets\*"
-del /Q "D:\Projects\ARA\1.0\src\Manager\ARAManager\ARAManager.Business\ARAManager.Business.Service\Ar_Data\*"
+del /Q "D:\Projects\ARA\1.0\src\Manager\ARAManager\ARAManager.Presentation\ARAManager.Presentation.Client\Ar_Data\Json\*"
 copy "D:\Projects\ARA\1.0\src\Manager\ARAManager\ARAManager.Presentation\ARAManager.Presentation.Client\Ara_Data\Test.jpg" "D:\Projects\ARA\1.0\src\Manager\ARAManager\ARAManager.Presentation\ARAManager.Presentation.Client\Ara_Data\Campaigns\Avatar"
 copy "D:\Projects\ARA\1.0\src\Manager\ARAManager\ARAManager.Presentation\ARAManager.Presentation.Client\Ara_Data\Test.jpg" "D:\Projects\ARA\1.0\src\Manager\ARAManager\ARAManager.Presentation\ARAManager.Presentation.Client\Ara_Data\Campaigns\Banner"
 copy "D:\Projects\ARA\1.0\src\Manager\ARAManager\ARAManager.Presentation\ARAManager.Presentation.Client\Ara_Data\Test.jpg" "D:\Projects\ARA\1.0\src\Manager\ARAManager\ARAManager.Presentation\ARAManager.Presentation.Client\Ara_Data\Missions\Avatar"
